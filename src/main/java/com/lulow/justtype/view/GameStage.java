5a33490b72112dd82409f50908f2f7d0fc432e4c
Lulow08 <yostinomgdan08@gmail.com>
2026-03-08 17:17:56 -0500

    add font loader

--- src/main/java/com/lulow/justtype/view/GameStage.java
+++ src/main/java/com/lulow/justtype/view/GameStage.java
@@ -4,18 +4,37 @@ import com.lulow.justtype.Main;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class GameStage {
     public GameStage(Stage gameStage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/fxml/game-view.fxml"));
+
+        loadFonts();
+
+        FXMLLoader fxmlLoader =
+                new FXMLLoader(getClass().getResource("/fxml/game-view.fxml"));
 
         Parent root = fxmlLoader.load();
+
         Scene scene = new Scene(root);
+
         gameStage.setTitle("JustType");
         gameStage.setScene(scene);
         gameStage.show();
+    }
+
+    private void loadFonts() {
+        Font.loadFont(
+                getClass().getResourceAsStream("/fonts/Determination-Regular.ttf"),
+                14
+        );
+
+        Font.loadFont(
+                getClass().getResourceAsStream("/fonts/GeistMonoNerdFont-Regular.otf"),
+                14
+        );
     }
 }
