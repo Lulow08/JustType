0c7ad9c8c65775076958a6b018c68b15b420eb43
Lulow08 <yostinomgdan08@gmail.com>
2026-03-09 08:06:49 -0500

    style: quick formatting change

--- src/main/java/com/lulow/justtype/controller/GameController.java
+++ src/main/java/com/lulow/justtype/controller/GameController.java
@@ -5,8 +5,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
 public class GameController {
-    @FXML
-    private TextField inputField;
+
+    @FXML private TextField inputField;
 
     @FXML
     private void onHandleValidate(ActionEvent actionEvent) {
