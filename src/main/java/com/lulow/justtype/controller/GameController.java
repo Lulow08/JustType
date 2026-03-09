70acc3920d6c69e453090d8b1141d3f23dd0314c
Lulow08 <yostinomgdan08@gmail.com>
2026-03-08 19:49:51 -0500

    feature: add first event

--- src/main/java/com/lulow/justtype/controller/GameController.java
+++ src/main/java/com/lulow/justtype/controller/GameController.java
@@ -1,4 +1,17 @@
 package com.lulow.justtype.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class GameController {
+    @FXML
+    private TextField inputField;
+
+    @FXML
+    private void onHandleValidate(ActionEvent actionEvent) {
+        String word = inputField.getText();
+        System.out.println(word);
+        inputField.clear();
+    }
 }
