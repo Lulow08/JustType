c8b3ab3a69717f87581dfc47f549f2a51755b917
Lulow08 <yostinomgdan08@gmail.com>
2026-03-16 14:41:29 -0500

    feature: add stop and setIntensity methods

--- src/main/java/com/lulow/justtype/view/particles/IParticleEmitter.java
+++ src/main/java/com/lulow/justtype/view/particles/IParticleEmitter.java
@@ -2,4 +2,6 @@ package com.lulow.justtype.view.particles;
 
 public interface IParticleEmitter {
     void play(double seconds);
+    void stop();
+    void setIntensity(int particlesPerFrame, double speedMultiplier);
 }
